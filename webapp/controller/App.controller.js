2af59cbe05ef886cf13225fd51f319ff04d8a761
Paulo Silva <paulo.hendrixpss@gmail.com>
2022-10-10 05:12:45 +0000

    Fontes modulo 06 aula 05

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("moovi.m06a05.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"moovi/m06a05/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
